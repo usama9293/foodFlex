5b002c296eab618c4b4385c08ff48440c4e8b2d4
usama9293 <usamaarshad9293@gmail.com>
2024-06-08 10:47:02 +0500

    "server and database created"

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+// Routes
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
